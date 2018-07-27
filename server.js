7ca04a61361f104bf4be3de2eed9c3c9957a7de1
ayuwidyainggit <37057557+ayuwidyainggit@users.noreply.github.com>
2018-07-27 11:28:12 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -6,11 +6,22 @@ app.set('view engine', 'pug')
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.use(express.static('public'))
+
 app.get('/ayu', function(req, res){
-   res.render('suplier');
+   res.render('suplier')
 }) 
 app.get('/ayu1', function(req, res){
-   res.render('form2');
+   res.render('hutang1')
+})
+app.post('/ayu1', function(req, res){
+   res.render('bphtb')
+}) 
+app.get('/person', function(req, res){
+   res.render('person')
 })   
+app.post('/ayuu', function(req, res){
+   res.render('form2')
+})  
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
